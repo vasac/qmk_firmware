54a7838b9d3b6fbe761f5d7305da405ab729ada4
Vaso Putica <vaso@putica.com>
2020-08-19 13:39:33 +0200

    +- PgUp PgDn \ ~

--- keyboards/kyria/keymaps/vasac/keymap.c
+++ keyboards/kyria/keymaps/vasac/keymap.c
@@ -49,62 +49,63 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * Base Layer: QWERTY
      *
      * ,-------------------------------------------.                              ,-------------------------------------------.
-     * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
+     * |  ` ~   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
      * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
      * |Ctrl/ESC|   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
      * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
-     * | LShift |   Z  |   X  |   C  |   V  |   B  | Tab  |LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? |  - _   |
+     * | LShift |   Z  |   X  |   C  |   V  |   B  | Tab  |LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? |  | \   |
      * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
-     *                        | Tab  | Alt  | GUI  | Space| Esc  |  | Enter| Space|RShift| | \  | AltGr|
+     *                        | Tab  | Alt  | GUI  | Space| Esc  |  | Enter| Space|RShift|  - _ | = +  |
      *                        |      |      |      | Lower| Raise|  | Lower| Raise|      |      |      |
      *                        `----------------------------------'  `----------------------------------'
      */
     [_QWERTY] = LAYOUT(
-                       KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+                       KC_GRAVE,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
                        MT(MOD_LCTL, KC_ESC),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-                       KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,   KC_LSFT, KC_LSFT, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                       KC_TAB, KC_LALT, KC_LGUI, LT(_LOWER, KC_SPC), LT(_RAISE, KC_ESC), LT(_LOWER, KC_ENT), LT(_RAISE, KC_SPC), KC_RSFT,  KC_BSLS, KC_RALT
+                       KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,   KC_LSFT, KC_LSFT, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+                       KC_TAB, KC_LALT, KC_LGUI, LT(_LOWER, KC_SPC), LT(_RAISE, KC_ESC), LT(_LOWER, KC_ENT), LT(_RAISE, KC_SPC), KC_RSFT,  KC_MINS, KC_EQL
                        ),
 
     /*
      * Lower Layer: Numbers, functions, symbols
      *
      * ,-------------------------------------------.                              ,-------------------------------------------.
-     * |   `    | 1 !  | 2 @  | 3 #  | 4 $  | 5 %  |                              | 6 ^  | 7 &  | 8 *  | 9 (  | 0 )  |  | \   |
+     * |        | 1 !  | 2 @  | 3 #  | 4 $  | 5 %  |                              | 6 ^  | 7 &  | 8 *  | 9 (  | 0 )  |        |
      * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
      * |        | !    | @    | #    | $    | %    |                              | ^    | &    | *    | (    | )    |        |
      * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
-     * |        | F1   | F2   | F3   | F4   | F5   |      |      |  |      |      | F6   | F7   | F8   | F9   | F10  |        |
+     * |        | F1   | F2   | F3   | F4   | F5   |      |      |  |      |      | F6   | F7   | F8   | F9   | F10  |  = +   |
      * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
-     *                        |      |      |      |      |      |  |      |      | (    | )    |      |
+     *                        |      |      |      |      |      |  |      |      | [    | ]    |      |
      *                        |      |      |      |      |      |  |      |      |      |      |      |
      *                        `----------------------------------'  `----------------------------------'
      */
     [_LOWER] = LAYOUT(
-                      KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+                      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
                       _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
                       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-                      _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______
+
+                      _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______
                       ),
 
     /*
      * Raise Layer: Number keys, media, navigation
      *
      * ,-------------------------------------------.                              ,-------------------------------------------.
-     * |  ~     |   1  |  2   |  3   |  4   |  5   |                              |  6   |  7   |  8   |  9   |  0   |        |
+     * |        |   1  |  2   |  3   |  4   |  5   |                              |  6   |  7   |  8   |  9   |  0   |        |
      * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
-     * |        |      | Prev | Play | Next | VolUp|                              | Left | Down | Up   | Right|      |        |
+     * |        |      | Prev | Play | Next | VolUp|                              | Left | Down | Up   | Right| PgUp | PgDn   |
      * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
-     * |        |      |      |      | Mute | VolDn|      |      |  |      |      | MLeft| Mdown| MUp  |MRight|      |        |
+     * |        |      |      |      | Mute | VolDn|      |      |  |      |      | MLeft| Mdown| MUp  |MRight| Home | End    |
      * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
      *                        |      |      |      |      |      |  |      |      | MClk1|      |      |
      *                        |      |      |      |      |      |  |      |      |      |      |      |
      *                        `----------------------------------'  `----------------------------------'
      */
     [_RAISE] = LAYOUT(
-                      KC_TILDE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-                      _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-                      _______, _______, _______, _______, KC_MUTE, KC_VOLD, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+                      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+                      _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGUP, KC_PGDN,
+                      _______, _______, _______, _______, KC_MUTE, KC_VOLD, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_HOME, KC_END,
                       _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______
                       ),
     /*
